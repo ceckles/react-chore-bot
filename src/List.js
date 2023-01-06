ff36b2250cda60968eaebaccc75281b6c277dffa
Chad Eckles <chad1020@gmail.com>
2023-01-06 02:41:35 -0600

    update image path and packages

--- src/List.js
+++ src/List.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
-import whooshSfx from './sounds/whoosh.flac';
+import React from "react";
+import whooshSfx from "./sounds/whoosh.flac";
 
-
-const List = ({chores, setChore, setCount}) => {
+const List = ({ chores, setChore, setCount }) => {
   //const [choreList, setChoreList] = useState(chores);
   const sfxWhoosh = new Audio(whooshSfx);
 
@@ -12,22 +11,32 @@ const List = ({chores, setChore, setCount}) => {
     setChore(array);
     setCount(array.length);
     sfxWhoosh.play();
-
-  }
+  };
 
   return (
     <>
       {chores.map((chore, index) => {
-        const { id, name, day, due,  image } = chore;
+        const { id, name, day, due, image } = chore;
         return (
-            <div key={id} className='chore'>
-                <img tabIndex="0" alt={name} src={image} className="chore-images" />
-                <div>
-                <h4 tabIndex="0">{name}</h4>
-                <p tabIndex="0">Due: {due}</p>
-                <div className="grow">
-                    <img tabIndex="0" src="../icons/delete-button.png" onClick={() => resolveChore(index)} className="remove-button" alt="remove chore" />   
-                </div>
+          <div key={id} className="chore">
+            <img
+              tabIndex="0"
+              alt={name}
+              src={`${process.env.PUBLIC_URL}` + image}
+              className="chore-images"
+            />
+            <div>
+              <h4 tabIndex="0">{name}</h4>
+              <p tabIndex="0">Due: {due}</p>
+              <div className="grow">
+                <img
+                  tabIndex="0"
+                  src="./icons/delete-button.png"
+                  onClick={() => resolveChore(index)}
+                  className="remove-button"
+                  alt="remove chore"
+                />
+              </div>
             </div>
           </div>
         );
--- src/data.js
+++ src/data.js
@@ -1,50 +1,50 @@
 export const data = [
-    {
-        id: 1,
-        name: 'Wash Dishes',
-        day: 'Sunday',
-        status: true,
-        image: '../icons/clean-dishes.png',
-        due:"2021-12-8 @ 18:52"
-    },
-    {
-        id: 2,
-        name: 'Sweep',
-        day: 'Saturday',
-        status: true,
-        image: '../icons/sweep.png',
-        due:"2021-12-9 @ 12:55"
-    },
-    {
-        id: 3,
-        name: 'Dust',
-        day: 'Tuesday',
-        status: true,
-        image: '../icons/dust.png',
-        due:"2021-12-09 @ 13:00"
-    },
-    {
-        id: 4,
-        name: 'Mop',
-        day: 'Saturday',
-        status: true,
-        image: '../icons/mop.png',
-        due:"2021-12-19 @ 11:00"
-    },
-    {
-        id: 5,
-        name: 'Take out trash',
-        day: 'Monday',
-        status: true,
-        image: '../icons/trash.png',
-        due:"2021-12-19 @ 09:00"
-    },
-    {
-        id: 6,
-        name: 'Laundry',
-        day: 'Wednesday',
-        status: true,
-        image: '../icons/washing.png',
-        due:"2021-12-20 @ 17:30"
-    },
+  {
+    id: 1,
+    name: "Wash Dishes",
+    day: "Sunday",
+    status: true,
+    image: "icons/clean-dishes.png",
+    due: "2021-12-8 @ 18:52",
+  },
+  {
+    id: 2,
+    name: "Sweep",
+    day: "Saturday",
+    status: true,
+    image: "icons/sweep.png",
+    due: "2021-12-9 @ 12:55",
+  },
+  {
+    id: 3,
+    name: "Dust",
+    day: "Tuesday",
+    status: true,
+    image: "./icons/dust.png",
+    due: "2021-12-09 @ 13:00",
+  },
+  {
+    id: 4,
+    name: "Mop",
+    day: "Saturday",
+    status: true,
+    image: "./icons/mop.png",
+    due: "2021-12-19 @ 11:00",
+  },
+  {
+    id: 5,
+    name: "Take out trash",
+    day: "Monday",
+    status: true,
+    image: "./icons/trash.png",
+    due: "2021-12-19 @ 09:00",
+  },
+  {
+    id: 6,
+    name: "Laundry",
+    day: "Wednesday",
+    status: true,
+    image: "./icons/washing.png",
+    due: "2021-12-20 @ 17:30",
+  },
 ];
